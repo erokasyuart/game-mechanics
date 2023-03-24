07d967eac0b1e04fefa698b1f4767a4042c93a0f
erokasyuart <erikat.stuart@gmail.com>
2023-03-24 14:06:57 +1300

    Bigger bullets code; needs debugging bc not funtional

--- Assets/Scripts/ShootEnemies.cs
+++ Assets/Scripts/ShootEnemies.cs
@@ -6,8 +6,8 @@ public class ShootEnemies : MonoBehaviour
 {
     public List<GameObject> enemiesInRange;
 
-    private float lastShotTime;
-    private MonsterData monsterData;
+    private float lastShotTime; //when the last bullet was fired
+    private MonsterData monsterData; //references the bullet type, firerate
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +20,28 @@ public class ShootEnemies : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        GameObject target = null;
+        float minimalEnemyDistance = float.MaxValue;
+        foreach(GameObject enemy in enemiesInRange)
+        {
+            float distanceToGoal = enemy.GetComponent<MoveEnemy>().DistanceToGoal();
+            if (distanceToGoal < minimalEnemyDistance)
+            {
+                target = enemy;
+                minimalEnemyDistance = distanceToGoal;
+            }
+
+            if (target != null)
+            {
+                if (Time.time - lastShotTime > monsterData.CurrentLevel.fireRate)
+                {
+                    Shoot(target.GetComponent<Collider2D>());
+                    lastShotTime = Time.time;
+                }
+                Vector3 direction = gameObject.transform.position - target.transform.position;
+                gameObject.transform.rotation = Quaternion.AngleAxis(Mathf.Atan2(direction.y, direction.x) * 180 /Mathf.PI, new Vector3(0,0,1));
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -45,5 +66,18 @@ public class ShootEnemies : MonoBehaviour
         Vector3 startPosition = gameObject.transform.position;
         Vector3 targetPosition = target.transform.position;
         startPosition.z = bulletPrefab.transform.localPosition.z;
+        targetPosition.z = bulletPrefab.transform.position.z;
+
+        GameObject newBullet = (GameObject)Instantiate(bulletPrefab);
+        newBullet.transform.position = startPosition;
+        BulletBehaviour bulletComp = newBullet.GetComponent<BulletBehaviour>();
+        bulletComp.target = target.gameObject;
+        bulletComp.startPosition = startPosition;
+        bulletComp.targetPosition = targetPosition;
+
+        Animator animator = monsterData.CurrentLevel.visualisation.GetComponent<Animator>();
+        animator.SetTrigger("fireShot");
+        AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+        audioSource.PlayOneShot(audioSource.clip);
     }
 }
