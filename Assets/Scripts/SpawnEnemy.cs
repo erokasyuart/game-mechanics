daae28e46eda9f283340fe496600628ad02d63bd
Erika Stuart <erikat.stuart@gmail.com>
2023-06-21 17:16:50 +1200

    Finally fixed wave 3 spawning enemies correctly

--- Assets/Scripts/SpawnEnemy.cs
+++ Assets/Scripts/SpawnEnemy.cs
@@ -5,11 +5,10 @@ using UnityEngine;
 [System.Serializable]
 public class Wave
 {
-    public GameObject enemyPrefab;
-    public GameObject enemy2Prefab;
-    
-    public float spawnInterval = 2;
+    public List<GameObject> enemyType;
+    public float spawnInterval = 1;
     public int maxEnemies = 20;
+    public List<int> enemyCount;
 }
 
 public class SpawnEnemy : MonoBehaviour
@@ -40,15 +39,27 @@ public class SpawnEnemy : MonoBehaviour
         {
             float timeInterval = Time.time - lastSpawnTime;
             float spawnInterval = waves[currentWave].spawnInterval;
+            List<int> enemyCount = waves[currentWave].enemyCount;
 
             if (((enemiesSpawned == 0 && timeInterval > timeBetweenWaves) //If no enemies have spawned and the time interval has ended
             || (enemiesSpawned != 0 && timeInterval > spawnInterval)) //or there are no enemies and the time passed is more than the enemy spawn interval
             && (enemiesSpawned < waves[currentWave].maxEnemies)) //and the enemies spawned are less than the current wave's max # of enemies
             {
-                lastSpawnTime = Time.time; //then reset the lastSpawnTime
-                GameObject newEnemy = (GameObject)Instantiate(waves[currentWave].enemyPrefab); //instantiate a new enemy
-                newEnemy.GetComponent<MoveEnemy>().waypoints = waypoints; //set their waypoints to the waypoints.
-                enemiesSpawned++; //increase the enemy spawn
+                if (enemiesSpawned <= enemyCount[0] - enemyCount[1]) //if the enemies that have spawned are less or equal to the number of desired enemy1 excluding the number of desired enemy2
+                {
+                    lastSpawnTime = Time.time; //then reset the lastSpawnTime
+                    GameObject newEnemy = (GameObject)Instantiate(waves[currentWave].enemyType[0]); //instantiate a new enemy
+                    enemiesSpawned++; //increase the enemy spawn
+                    newEnemy.GetComponent<MoveEnemy>().waypoints = waypoints; //set their waypoints to the waypoints.
+                }
+                else
+                {
+                    lastSpawnTime = Time.time; //then reset the lastSpawnTime
+                    enemiesSpawned++; //increase the enemy spawn
+                    GameObject newEnemy1 = (GameObject)Instantiate(waves[currentWave].enemyType[1]); //instantiate enemy2
+                    newEnemy1.GetComponent<MoveEnemy>().waypoints = waypoints; //set their waypoints to the waypoints.
+                }
+
             }
             //if there are max number of enemies and there are no enemies with a tag (aka, all gone!)
             if (enemiesSpawned == waves[currentWave].maxEnemies && GameObject.FindGameObjectWithTag("Enemy") == null)
